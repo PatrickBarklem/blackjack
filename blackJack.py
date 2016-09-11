a6c39611650b529361dad88da96371db7d9a7a58
PatrickBarklem <patrick_96@hotmail.co.uk>
2016-09-11 05:49:23 +0100

    Add files via upload

--- blackJack.py
+++ blackJack.py
@@ -6,27 +6,59 @@ Time for blackjack and hookers*
 from random import shuffle
 
 class game:
-    def __init__(self, noPlayers):
-        self.nP = noPlayers #decide how many players will be playing
+    def __init__(self, numPlayers):
+        self.nP = numPlayers
+        self.gameDeck = deck()
+        self.gameDeck.createNewDeck()
 
+    def startRound(self):
+        self.groupPlayers = []
+        for i in range(self.nP):
+            newPlayer = player(i)
+            self.groupPlayers.append(newPlayer)
+        for x in range(self.nP):
+            for y in range(2):
+                self.groupPlayers[x].receiveCard(self.gameDeck.dealCard())
+            
 class deck:
     def __init__(self):
         self.suit = ["Clubs","Diamonds","Hearts","Spades"]
         self.card = []
-        self.deck = []
+        self.deckContents = []
 
-    def createDeck(self):
-        #Creates a basic deck and shuffles it
+    def createNewDeck(self):
+        #Creates a new deck and shuffles it
+        self.deck = []
         for i in range(4):
             for x in range(1,14):
                 self.card = [self.suit[i],x]
-                self.deck.append(self.card)
-        shuffle(self.deck)
+                self.deckContents.append(self.card)
+        shuffle(self.deckContents)
+
+    def printDeck(self):
+        #prints deck for testing
+        print (self.deckContents)
+
+    def dealCard(self):
+        #removes card from deck and returns value
+        return self.deckContents.pop()
+    
+class player:
+    def __init__(self,number):
+        self.hand = []
+        self.name = "Player: "+str((number+1))
+
+    def printName(self):
+        #test to see if object naming works
+        print (self.name)
+
+    def receiveCard(self,card):
+        #add next card in deck to hand contents
+        self.hand.append(card)
+        print(self.name+str(self.hand)) #testing
         
-
-    def dealHand(self):
-        return none
-
-'''class player:
-    return none '''
+if __name__ == "__main__":
+    test = game(4)
+    test.startRound()
+    
     
